04dde0c0616b041330a8a89ea7f32eb348f689de
JiiXaa <anythingfx@gmail.com>
2023-10-15 22:05:00 +0100

    Signup form working on submit to a development version of DRF API. All server validation also added.

--- src/pages/auth/SignUpForm.jsx
+++ src/pages/auth/SignUpForm.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import styles from '../../styles/SignInUpForm.module.css';
 import btnStyles from '../../styles/Button.module.css';
@@ -7,16 +7,55 @@ import appStyles from '../../App.module.css';
 
 import cameraImg from '../../assets/camera001.webp';
 
-import { Form, Button, Image, Col, Row, Container } from 'react-bootstrap';
+import {
+  Form,
+  Button,
+  Image,
+  Col,
+  Row,
+  Container,
+  Alert,
+} from 'react-bootstrap';
+import axios from 'axios';
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: '',
+    password1: '',
+    password2: '',
+  });
+
+  const { username, password1, password2 } = signUpData;
+
+  const [errors, setErrors] = useState({});
+
+  let navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setSignUpData({ ...signUpData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post(
+        'http://localhost:8000/dj-rest-auth/registration/',
+        signUpData
+      );
+      navigate('/signin');
+    } catch (error) {
+      setErrors(error.response?.data);
+    }
+  };
+
   return (
     <Row className={styles.Row}>
       <Col className='my-auto py-2 p-md-2' md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
 
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId='username'>
               <Form.Label className='d-none'>Username</Form.Label>
               <Form.Control
@@ -24,8 +63,16 @@ const SignUpForm = () => {
                 type='text'
                 placeholder='Username'
                 name='username'
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
+
+            {errors.username?.map((errorMsg, idx) => (
+              <Alert key={idx} variant='warning'>
+                {errorMsg}
+              </Alert>
+            ))}
 
             <Form.Group controlId='password1'>
               <Form.Label className='d-none'>Password</Form.Label>
@@ -34,8 +81,16 @@ const SignUpForm = () => {
                 type='password'
                 placeholder='Password'
                 name='password1'
+                value={password1}
+                onChange={handleChange}
               />
             </Form.Group>
+
+            {errors.password1?.map((errorMsg, idx) => (
+              <Alert key={idx} variant='warning'>
+                {errorMsg}
+              </Alert>
+            ))}
 
             <Form.Group controlId='password2'>
               <Form.Label className='d-none'>Confirm password</Form.Label>
@@ -44,8 +99,16 @@ const SignUpForm = () => {
                 type='password'
                 placeholder='Confirm password'
                 name='password2'
+                value={password2}
+                onChange={handleChange}
               />
             </Form.Group>
+
+            {errors.password2?.map((errorMsg, idx) => (
+              <Alert key={idx} variant='warning'>
+                {errorMsg}
+              </Alert>
+            ))}
 
             <Button
               className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
@@ -53,6 +116,12 @@ const SignUpForm = () => {
             >
               Submit
             </Button>
+
+            {errors.non_field_errors?.map((errorMsg, idx) => (
+              <Alert key={idx} variant='warning' className='mt-3'>
+                {errorMsg}
+              </Alert>
+            ))}
           </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
