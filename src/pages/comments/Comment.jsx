1d80dcb7bca249f791a61b117c11b80ee371f55b
JiiXaa <anythingfx@gmail.com>
2024-01-04 22:53:53 +0000

    Comment feature add: added dropdown for deleting and editing a comment. Integrated logic for deleting comment and updating comment state and count.

--- src/pages/comments/Comment.jsx
+++ src/pages/comments/Comment.jsx
@@ -2,9 +2,44 @@ import { Card, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Avatar from '../../components/Avatar';
 import styles from '../../styles/Comment.module.css';
+import { MoreDropdown } from '../../components/MoreDropdown';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { axiosRes } from '../../api/axiosDefaults';
 
 const Comment = (props) => {
-  const { profile_id, profile_image, owner, content, updated_at } = props;
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    content,
+    updated_at,
+    id,
+    setPost,
+    setComments,
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+
+      setComments((prevComments) => ({
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
@@ -22,6 +57,9 @@ const Comment = (props) => {
               <span className={styles.Date}>{updated_at}</span>
               <p>{content}</p>
             </Card.Body>
+          </Col>
+          <Col xs='auto' className='ms-auto'>
+            <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
           </Col>
         </Row>
       </Card>
